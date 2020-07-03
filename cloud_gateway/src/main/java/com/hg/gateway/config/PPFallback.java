9f9a7c12efc68b2e0506bdf62088159fc596f964
haofa.zhong <841018243@qq.com>
2020-07-03 18:04:31 +0800

    gateway网关配置修改，已完成

--- cloud_gateway/src/main/java/com/hg/gateway/config/PPFallback.java
+++ cloud_gateway/src/main/java/com/hg/gateway/config/PPFallback.java
@@ -1,7 +1,81 @@
 package com.hg.gateway.config;
 
+import com.alibaba.fastjson.JSONObject;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * 熔断降级服务
  */
-public class PPFallback {
+@Component
+public class PPFallback implements FallbackProvider {
+    /**
+     * return 的值为Null 或者 * 代表全部路由 ，如果填服务的话，必须是服务名小写
+     * @return
+     */
+    @Override
+    public String getRoute() {
+        return null;
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        String message = "";
+        if (cause instanceof HystrixTimeoutException) {
+            message = "Timeout";
+        } else {
+            message = "Service exception";
+        }
+        return fallbackResponse(message);
+    }
+
+    public ClientHttpResponse fallbackResponse(String message) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                JSONObject r = new JSONObject();
+                r.put("code", "9999");
+                r.put("msg", message);
+                r.put("data","");
+                return new ByteArrayInputStream(r.toJSONString().getBytes("UTF-8"));
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+
 }
